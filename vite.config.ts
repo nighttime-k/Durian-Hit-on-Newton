1ec67bd0870eca61e5e67cd39345e655367e60eb
nighttime-k <277295511+nighttime-k@users.noreply.github.com>
2026-09-16 00:25:27 +0700

    chore: add Vite config

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  server: { host: "0.0.0.0", port: 8080, strictPort: true },
+  resolve: { tsconfigPaths: true },
+  plugins: [tailwindcss(), tanstackStart(), viteReact()],
+});
